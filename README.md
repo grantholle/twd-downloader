3fae2f240c73c2cc92a884c54ac54b4c3cc498c1
Grant Holle <hollegrant@gmail.com>
2017-04-18 13:03:01 +0800

    Walks and logs correctly, adding issue range capability

--- README.md
+++ README.md
@@ -3,15 +3,23 @@
 Downloads TWD comics from [OMG.BEAU.PEEP](http://www.omgbeaupeep.com).
 
 ```
-$ twd --help
+$ twd -h
 
   Usage: twd [options]
 
   Options:
 
-    -h, --help       output usage information
-    -V, --version    output the version number
-    -i, --issue      The issue number at which you wish to start
-    -d, --directory  The directory in which you wish to put the comic files
-    -s --save        Save the directory configuration
+    -h, --help              output usage information
+    -V, --version           output the version number
+    -i, --issue <n>         The issue number at which to start. If none is included, it will start from the last issue downloaded
+    -e, --end <n>           The issue at which to stop
+    -n, --no-walk           Only get the single issue
+    -d, --directory <path>  The directory in which you wish to put the comic files. Supersedes saved path.
+    -s, --save              Save the directory configuration
+```
+
+## Installation
+
+```
+$ npm i -g twd-comic-downloader
 ```
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const p = require('path'),
       winston = require('winston'),
       program = require('commander'),
       config = require('./config.json'),
+      jsonfile = require('jsonfile'),
       imgBase = "http://www.omgbeaupeep.com/comics/",
 
       getIssueNumber = url => {
@@ -33,14 +34,13 @@ const p = require('path'),
         return s;
       },
 
-      download = (uri, filename, callback) => {
+      download = (uri, filename, cb) => {
         request.head(uri, (err, res, body) => {
-          if (err) {
-            console.log('Error!'.red, e)
-          }
+          if (err)
+            return winston.error(`Error downloading '${uri}'`, e)
 
           fs.mkdir(p.dirname(filename), function (e) {
-            request(uri).pipe(fs.createWriteStream(filename)).on('close', callback)
+            request(uri).pipe(fs.createWriteStream(filename)).on('close', cb)
           })
         })
       },
@@ -53,35 +53,93 @@ const p = require('path'),
           }
 
           const $ = cheerio.load(res.toString()),
-              imgPath = imgBase + $('img.picture').attr('src'),
-              savePath = p.join(saveBase, getIssueNumber(url).toString(), pad(getPageNumber(url)) + '.jpg')
+                imgPath = imgBase + $('img.picture').attr('src'),
+                issue = getIssueNumber(url),
+                savePath = p.join(program.directory, issue.toString(), pad(getPageNumber(url)) + '.jpg')
 
           if (imgPath) {
-            const nextUrl = $('img.picture').parent().attr('href')
+            const nextUrl = $('img.picture').parent().attr('href'),
+                  nextIssue = getIssueNumber(imgBase + nextUrl)
 
-            fs.stat(savePath, (err, stats) => {
-              if (err) {
-                winston.info(`Downloading ${imgPath}`)
+            // fs.stat(savePath, (err, stats) => {
+              // if (err) {
+                winston.info(`Starting download of ${imgPath}`)
 
                 download(imgPath, savePath, () => {
-                  winston.info(`Saved ${savePath}!`)
+                  let keepGoing = true
 
-                  if (nextUrl)
+                  winston.info(`Image saved to ${savePath}`)
+
+                  if (nextIssue !== issue) {
+                    winston.info(`Issue ${issue} has finished downloading`)
+                    keepGoing = (!!program.end && nextIssue <= program.end) && !program.noWalk
+                  }
+
+                  if (nextUrl && keepGoing)
                     crawl(imgBase + nextUrl)
                   else
                     winston.info('Crawl complete!')
                 })
-              }
-            })
+              // }
+            // })
+          } else {
+            winston.info('Crawl complete!')
           }
         })
+      },
+
+      start = () => {
+        let baseUrl = 'http://www.omgbeaupeep.com/comics/The_Walking_Dead/$issue/'
+
+        winston.info('Starting crawl...')
+
+        // Intellegently guess the starting issue
+        if (program.issue) {
+          baseUrl = baseUrl.replace('$issue', program.issue)
+          crawl(baseUrl)
+        } else {
+          fs.readdir(program.directory, (err, issues) => {
+            if (err)
+              return winston.error(`There was a problem with the path '${program.directory}'. Please check the path and try again`, err)
+
+            issues = issues.filter(i => !(/(^|\/)\.[^\/\.]/g).test(i))
+              .map(i => parseInt(i, 10))
+
+            const issue = Math.max.apply(Math, issues) + 1
+            baseUrl = baseUrl.replace('$issue', issue)
+
+            crawl(baseUrl)
+          })
+        }
       };
 
+// Add colors and pretty stuff to console logging
+winston.cli()
+
+// Set up the cli program
 program.version(require('./package.json').version)
-  .option('-i, --issue', 'The issue number at which you wish to start')
-  .option('-d, --directory', 'The directory in which you wish to put the comic files')
-  .option('-s --save', 'Save the directory configuration')
+  .option('-i, --issue <n>', 'The issue number at which to start. If none is included, it will start from the last issue downloaded', parseInt)
+  .option('-e, --end <n>', 'The issue at which to stop', parseInt)
+  .option('-n, --no-walk', 'Only get the single issue')
+  .option('-d, --directory <path>', 'The directory in which you wish to put the comic files. Supersedes saved path.', config.directory)
+  .option('-s, --save', 'Save the directory configuration')
   .parse(process.argv)
 
-winston.info('Starting crawl...')
-crawl(urlBase)
+// Check to make sure a directory exists
+if (!program.directory) {
+  return winston.error('Please include a directory in which you wish to put the files')
+}
+
+// Save the directory config file
+if (program.directory && program.save) {
+  jsonfile.spaces = 2
+
+  jsonfile.writeFile('./config.json', { directory: program.directory }, err => {
+    if (err)
+      return winston.error(`Error writing config file to ${p.join(__dirname, 'config.json')}`)
+
+    winston.info('Saved directory configuration')
+  })
+}
+
+start()
